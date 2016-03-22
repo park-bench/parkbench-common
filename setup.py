b6e36cab716f7c398404d4064b09be9ff7abbfac
Andrew Klapp <lithium@null.net>
2016-03-21 20:36:04 -0500

    Finally fixed the packaging.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+import setuptools
+
+setuptools.setup(name='confighelper',
+    version='0.1',
+    description='Parkbench configuration praser',
+    author='Joel Allen Luellwitz and Andrew Klapp',
+    author_email='eviljoel@linux.com, andklapp@null.net',
+    license='GPLv3',
+    packages=['confighelper'],
+    zipsafe=False)
